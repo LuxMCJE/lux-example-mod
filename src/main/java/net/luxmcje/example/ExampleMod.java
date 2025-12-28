01ced6147a7c3596ee92f8ab4bee71bf46a07eeb
dabdob-craft <dabdobcraft2@gmail.com>
2025-12-28 11:28:14 +0100

    Create ExampleMod.java

---
+++ src/main/java/net/luxmcje/example/ExampleMod.java
@@ -0,0 +1,12 @@
+package net.luxmcje.example;
+
+import net.luxmcje.loader.LuxLoader;
+
+public class ExampleMod {
+    public static void init() {
+        System.out.println("---------------------------------------");
+        System.out.println("   Hello from the first Lux Mod!       ");
+        System.out.println("   Running on LuxLoader v0.1.0         ");
+        System.out.println("---------------------------------------");
+    }
+}
